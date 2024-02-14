1e3fabde1cdcea2112b9d00c65bcbfd2c8a72b6d
Shubham Vanani <shubhamvanani@gmail.com>
2024-02-15 00:51:53 +0530

    NEW Donation UI Created

--- app/src/main/java/com/example/lagani20/Dashboard/DonorDashboard.java
+++ app/src/main/java/com/example/lagani20/Dashboard/DonorDashboard.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.example.lagani20.Modules.ADDDonations;
 import com.example.lagani20.R;
 import com.example.lagani20.RegisterLogin.MainActivity;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,12 +16,15 @@ import com.google.firebase.auth.FirebaseAuth;
 public class DonorDashboard extends AppCompatActivity {
 
     View logout;
+    ImageButton donationbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_dashboard);
 
         logout = findViewById(R.id.rectangle_6);
+        donationbtn = findViewById(R.id.rectangle_3);
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +33,14 @@ public class DonorDashboard extends AppCompatActivity {
                 Toast.makeText(DonorDashboard.this, "Logged Out Successfully", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(DonorDashboard.this,MainActivity.class));
                 overridePendingTransition(R.anim.push_up_in,R.anim.push_down_out);
+            }
+        });
+
+        donationbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DonorDashboard.this, ADDDonations.class));
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
             }
         });
     }
---
+++ app/src/main/java/com/example/lagani20/Modules/ADDDonations.java
@@ -0,0 +1,16 @@
+package com.example.lagani20.Modules;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lagani20.R;
+
+public class ADDDonations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adddonations);
+    }
+}
